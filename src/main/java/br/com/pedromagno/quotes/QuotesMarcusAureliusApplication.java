6cf77e7d435378a1a9f899836b0c19352d449836
Pedro Magno <pedromagnopro@gmail.com>
2024-08-14 00:21:06 -0300

    chore(init): initial project

---
+++ src/main/java/br/com/pedromagno/quotes/QuotesMarcusAureliusApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedromagno.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesMarcusAureliusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesMarcusAureliusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedromagno/quotes/QuotesMarcusAureliusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedromagno.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesMarcusAureliusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
